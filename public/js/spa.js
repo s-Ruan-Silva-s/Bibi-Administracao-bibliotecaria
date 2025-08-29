dfb9f84a6c26d449f3f088780b5902a32e5d0069
Ruan Silva <ruan.s.abc@gmail.com>
2025-08-29 19:19:05 -0300

    adição do painel de cadastro de livro

--- public/js/spa.js
+++ public/js/spa.js
@@ -1,4 +1,4 @@
-
+// <MOSTRAR AS SEÇÕES DE LIVROS, USUARIOS E EMPRESTIMOS>
 const cards = document.querySelectorAll(".home-area-main-sec-card");
 const panels = document.querySelectorAll(".home-area-main-panel");
 
@@ -6,9 +6,29 @@ cards.forEach(card => {
     card.addEventListener("click", () => {
         const alvo = card.dataset.panel;
         const painel = document.getElementById(alvo);
-
+        
         panels.forEach(p => p.classList.remove("ativo"));
         painel.classList.add("ativo");
     });
 });
+//--- MOSTRAR AS SEÇÕES DE LIVROS, USUARIOS E EMPRESTIMOS ---
 
+// <MOSTRAR AS SEÇÕES DE CADASTRO ESPECÍFICAS>
+
+const form = document.querySelector('#areaCadLivros form');
+
+const areaCadLivros = document.getElementById('areaCadLivros');
+
+function exibirCadastroLivros(){
+    areaCadLivros.classList.add('displayOn');
+}
+function fecharCadastroLivros(){
+    areaCadLivros.classList.remove('displayOn');
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  console.log("Enviando dados...");
+
+});
+//--- MOSTRAR AS SEÇÕES DE CADASTRO ESPECÍFICAS ---
